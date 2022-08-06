28769b7922adeb2476463582fc2f90aefaf528ec
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-08-06 14:00:02 -0400

    Add Hamburger animation

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,9 +1,30 @@
+// Import React Tools
+import { useState } from "react";
+
 // Import CSS
 import styles from "./Hamburger.module.css";
 
 const Hamburger = () => {
+  const [open, setOpen] = useState(false);
+
+  const openMenu = (e) => {
+    const target = e.target.closest(".hamMenu");
+    // console.log(target);
+    setOpen(!open);
+
+    if (open) {
+      target.children[0].classList = `${styles.bar} ${styles.barTop} ${styles.barTop_close}`;
+      target.children[1].classList = `${styles.bar} ${styles.barMid} ${styles.barMid_close}`;
+      target.children[2].classList = `${styles.bar} ${styles.barBot} ${styles.barBot_close}`;
+    } else {
+      target.children[0].classList = `${styles.bar} ${styles.barTop}`;
+      target.children[1].classList = `${styles.bar} ${styles.barMid}`;
+      target.children[2].classList = `${styles.bar} ${styles.barBot}`;
+    }
+  };
+
   return (
-    <div className={styles.Hamburger}>
+    <div onClick={openMenu} className={`hamMenu ${styles.Hamburger}`}>
       <div className={`${styles.bar} ${styles.barTop}`}></div>
       <div className={`${styles.bar} ${styles.barMid}`}></div>
       <div className={`${styles.bar} ${styles.barBot}`}></div>
